8c1db56273689e0034ae99cce9bf8ec190f4e26b
André Rodrigues <andre.rodrigues98@outlook.com>
2022-02-21 23:47:54 -0300

    adding breadth first search

---
+++ graph/index.js
@@ -0,0 +1,38 @@
+const Queue = require("../utils/queue");
+
+const graph = new Map();
+graph.set('you', ['alice', 'bob', 'claire']);
+graph.set('bob', ['anuj', 'peggy']);
+graph.set('alice', ['peggy']);
+graph.set('claire', ['thom', 'jonny']);
+graph.set('anuj', []);
+graph.set('peggy', []);
+graph.set('thom', []);
+graph.set('tonny', []);
+
+const personAndSeller = ({ name }) => {
+    return name[name.length - 1] === 'm';
+}
+
+const breadthFirstSearch = ({ graph }) => {
+    const searchQueue = new Queue();
+    graph.get('you').map(person => searchQueue.enqueue(person));
+    const verified = [];
+
+    while (searchQueue) {
+        const person = searchQueue.dequeue();
+        if (!verified.includes(person)) {
+            if (personAndSeller({ name: person })) {
+                console.log(`${person} is a mango seller.`);
+                return true;
+            } else {
+                graph.get(person).map(person => searchQueue.enqueue(person));
+                verified.push(person);
+            }
+        }
+    }
+
+    return false;
+}
+
+breadthFirstSearch({ graph });
