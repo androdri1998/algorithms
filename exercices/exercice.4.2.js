774ea5fcc79f45d51abf92cc2bac0e20c91ac742
André Rodrigues <andre.rodrigues98@outlook.com>
2023-09-10 18:06:15 -0300

    refactor: improving semantic of exercise 4.2

--- exercices/exercice.4.2.js
+++ exercices/exercice.4.2.js
@@ -1,9 +1,21 @@
+const isEmptyArray = (array) => array.length === 0;
+const removeLastItem = (array) => {
+    const cloneArray = [];
+    for(let index = 0; index <= array.length - 2; index++) {
+        cloneArray[index] = array[index];
+    }
+
+    return cloneArray;
+};
+
 const count = (array) => {
-    if (array[0] === undefined) {
+    if (isEmptyArray(array)) {
         return 0;
     }
 
-    return 1 + count(array.slice(0, -1));
+    const nextArray = removeLastItem(array);
+
+    return 1 + count(nextArray);
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
