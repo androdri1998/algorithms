f7b0d2c55d7af77172b5d36239ca8182195dfaf4
André Rodrigues <andre.rodrigues98@outlook.com>
2023-09-10 19:55:09 -0300

    refactor: improving semantic to quicksort algorithm

--- quick_sort/index.js
+++ quick_sort/index.js
@@ -1,13 +1,40 @@
+const drawPosition = (array) => Math.floor(Math.random() * array.length);
+const isFirstNumberGreaterThanSecondNumber = (first, second) => first > second;
+const getMinors = (array, drawnNumber) => {
+    const minors = [];
+    for(let index = 0; index <= array.length - 1; index++) {
+        const itemValue = array[index];
+        if(isFirstNumberGreaterThanSecondNumber(drawnNumber, itemValue)) {
+            minors.push(itemValue);
+        }
+    }
+
+    return minors;
+}
+
+const getGreaters = (array, drawnNumber) => {
+    const greaters = [];
+    for(let index = 0; index <= array.length - 1; index++) {
+        const itemValue = array[index];
+        if(isFirstNumberGreaterThanSecondNumber(itemValue, drawnNumber)) {
+            greaters.push(itemValue);
+        }
+    }
+
+    return greaters;
+}
+
 const quickSort = (array) => {
-    if (array.length < 2) {
+    if (isFirstNumberGreaterThanSecondNumber(2, array.length)) {
         return array;
     }
 
-    const randomPosition = Math.floor(Math.random() * array.length);
-    const pivo = array[randomPosition];
-    const minors = array.filter(item => item < pivo);
-    const bigger = array.filter(item => item > pivo);
-    return quickSort(minors).concat([pivo].concat(quickSort(bigger)));
+    const randomPosition = drawPosition(array);
+    const pivot = array[randomPosition];
+    const minors = getMinors(array, pivot);
+    const greaters = getGreaters(array, pivot);
+
+    return [...quickSort(minors), pivot, ...quickSort(greaters)];
 }
 
 console.log(quickSort([10, 5, 2, 3, 6]));
