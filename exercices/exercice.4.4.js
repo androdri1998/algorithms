fd0e707d6b1c87ca298d3dd9013aa6508bbe47ca
André Rodrigues <andre.rodrigues98@outlook.com>
2023-09-10 19:17:00 -0300

    refactor: improving semantic of exercise 4.4

--- exercices/exercice.4.4.js
+++ exercices/exercice.4.4.js
@@ -1,22 +1,28 @@
+const isFirstNumberGreaterThanSecondNumber = (first, second) => first > second; 
+const isNumbersEquals = (first, second) => first === second; 
+const calcMiddleIndex = (smallerNumber, biggerNumber) => Math.floor((smallerNumber + biggerNumber) / 2);
+
 const binarySearch = (findNumber, array, smaller, bigger) => {
-    if (smaller > bigger) return null;
+    if (isFirstNumberGreaterThanSecondNumber(smaller, bigger)) return null;
     const biggerNumber = bigger || array.length - 1;
     const smallerNumber = smaller || 0;
 
-    const middle = Math.floor((smallerNumber + biggerNumber) / 2);
+    const middle = calcMiddleIndex(smallerNumber, biggerNumber);
+    const middleValue = array[middle];
 
-    if (array[middle] === findNumber) {
+    if (isNumbersEquals(middleValue, findNumber)) {
         return middle;
     }
 
-    if (findNumber > array[middle]) {
+    if (isFirstNumberGreaterThanSecondNumber(findNumber, middleValue)) {
         return binarySearch(findNumber, array, middle + 1, biggerNumber);
     }
-    if (findNumber < array[middle]) {
+
+    if (isFirstNumberGreaterThanSecondNumber(middleValue, findNumber)) {
         return binarySearch(findNumber, array, smallerNumber, middle - 1);
     }
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-const position = binarySearch(4, array);
+const position = binarySearch(10, array);
 console.log(position, array[position]);
