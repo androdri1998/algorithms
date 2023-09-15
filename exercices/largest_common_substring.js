4ade583e9f7a6931ffb310b992a8f458b67c2426
André Rodrigues <andre.rodrigues98@outlook.com>
2023-09-15 14:56:36 -0300

    feat: adding largest common substring problem solution

---
+++ exercices/largest_common_substring.js
@@ -0,0 +1,4 @@
+const largestCommonString = require("../largest_common_substring/largest_common_substring");
+
+console.log(largestCommonString('relevo', 'relogio'));
+console.log(largestCommonString('reloguo', 'relogio'));
---
+++ largest_common_substring/largest_common_substring.js
@@ -0,0 +1,35 @@
+const largestCommonString = (targetWord, compareWord) => {
+    const compareWordLength = compareWord.length;
+    const targetWordLength = targetWord.length;
+    const tableWord = [];
+    let largestCommonStringNumber = 0
+
+    for(let line = 0; line <= compareWordLength - 1; line++) {
+        for(let column = 0; column <= targetWordLength - 1; column++) {
+            if(!tableWord[line]) {
+                tableWord[line] = [];
+            }
+            if(!tableWord[line][column]) {
+                tableWord[line][column] = [];
+            }
+
+            if(compareWord[line] === targetWord[column]) {
+                const columnAbove = column - 1 < 0 ? 0 : column - 1;
+                const leftLine = line - 1 < 0 ? 0 : line - 1;
+                const newLargestCommonString = parseInt((tableWord[leftLine][columnAbove] || 0) + 1);
+                
+                tableWord[line][column] = newLargestCommonString;
+                largestCommonStringNumber = 
+                    newLargestCommonString > largestCommonStringNumber
+                    ? newLargestCommonString
+                    : largestCommonStringNumber;
+            } else {
+                tableWord[line][column] = 0;
+            }
+        }
+    }
+
+    return largestCommonStringNumber;
+}
+
+module.exports = largestCommonString;
